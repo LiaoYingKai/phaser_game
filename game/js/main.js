3ef21314036e940abb818493ff8ad77cab6d4503
Kai <b10223052@yuntech.org.tw>
2018-10-05 21:34:59 +0800

    preload all image

--- game/js/main.js
+++ game/js/main.js
@@ -12,13 +12,22 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('sky', './assets/sky.png')
-
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude',
+    'assets/dude.png', {
+      frameWidth: 32,
+      frameHeight: 48
+    }
+  );
 }
 
 function create() {
   // this.add.image(400, 0, 'sky')
   this.add.image(0, 0, 'sky').setOrigin(0, 0)
+  this.add.image(400, 300, 'dude')
 }
 
 function update() {}
