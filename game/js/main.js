bf6664357e4f819de5063fbdcda87ccf1e4215a4
Kai <b10223052@yuntech.org.tw>
2018-10-05 20:54:49 +0800

    load and create first object

--- game/js/main.js
+++ game/js/main.js
@@ -11,8 +11,14 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image('sky', './assets/sky.png')
 
-function create() {}
+}
+
+function create() {
+  // this.add.image(400, 0, 'sky')
+  this.add.image(0, 0, 'sky').setOrigin(0, 0)
+}
 
 function update() {}
