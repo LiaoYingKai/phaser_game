cf7a085d6ca988c647c89649625b1be1429fbd60
Kai <b10223052@yuntech.org.tw>
2018-10-06 22:45:55 +0800

    fix create in platforms

--- game/js/main.js
+++ game/js/main.js
@@ -41,7 +41,8 @@ function create() {
 
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-  platforms.create(600, 400, 'ground');
+  platforms.create(400, 100, 'ground').setScale(0.5);
+  platforms.create(600, 400, 'ground').setScale(1.5);
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 }
