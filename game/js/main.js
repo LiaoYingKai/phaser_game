b4d6d34ef1473376a5faf204483ee2cd32ae7add
Kai <b10223052@yuntech.org.tw>
2018-10-07 00:29:24 +0800

    create player and stand on ground

--- game/js/main.js
+++ game/js/main.js
@@ -45,6 +45,42 @@ function create() {
   platforms.create(600, 400, 'ground').setScale(1.5);
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{
+      key: 'dude',
+      frame: 4
+    }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 5,
+      end: 8
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  player.body.setGravityY(300);
+  this.physics.add.collider(player, platforms);
 }
 
 function update() {}
