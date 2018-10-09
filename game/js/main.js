315df2d1ca6c4a7ad88c40a48d197914290f91e4
Kai <b10223052@yuntech.org.tw>
2018-10-09 16:50:05 +0800

    create star

--- game/js/main.js
+++ game/js/main.js
@@ -4,12 +4,12 @@ var config = {
   height: 600,
   physics: {
     default: 'arcade',
-    // arcade: {
-    //   gravity: {
-    //     y: 300
-    //   },
-    //   debug: false
-    // }
+    arcade: {
+      gravity: {
+        y: 300
+      },
+      debug: false
+    }
   },
   scene: {
     preload: preload,
@@ -79,29 +79,43 @@ function create() {
     frameRate: 10,
     repeat: -1
   });
-  player.body.setGravityY(300);
-  this.physics.add.collider(player, platforms);
   cursors = this.input.keyboard.createCursorKeys();
 
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: {
+      x: 12,
+      y: 0,
+      stepX: 70
+    }
+  });
+
+  stars.children.iterate(function(child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update() {
-
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
-
     player.anims.play('left', true);
   } else if (cursors.right.isDown) {
     player.setVelocityX(160);
-
     player.anims.play('right', true);
   } else {
     player.setVelocityX(0);
-
     player.anims.play('turn');
   }
-
   if (cursors.up.isDown && player.body.touching.down) {
     player.setVelocityY(-330);
   }
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
 }
