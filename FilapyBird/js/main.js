60985e227aa2ab3be3b950c23066f92c79ceb8ac
Kai <b10223052@yuntech.org.tw>
2018-10-13 19:13:39 +0800

    create FilapyBird file

---
+++ FilapyBird/js/main.js
@@ -0,0 +1,23 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 300,
+  height: 500,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+var game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
