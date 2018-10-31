b3bd03166dfcb87fe59d7af6c2ba9ba6b8c01b0d
Kai <b10223052@yuntech.org.tw>
2018-11-01 00:23:18 +0800

    can stop game when have winner

--- Gomoku/js/main.js
+++ Gomoku/js/main.js
@@ -11,22 +11,10 @@ var config = {
 
 var game = new Phaser.Game(config)
 
-var checkerboard = createCheckerboard()
 var camp
 var self
 var socket
 var status = 'o'
-
-function createCheckerboard() {
-  let checkerboard = []
-  for (let i = 0; i < 20; i++) {
-    checkerboard[i] = new Array()
-    for (let j = 0; j < 20; j++) {
-      checkerboard[i][j] = 0
-    }
-  }
-  return checkerboard
-}
 
 function drawCheckerboard(graphics) {
   graphics.lineStyle(1, 0xffffff, 1)
@@ -58,27 +46,34 @@ function create() {
     if (status) {
       self.input.on('pointerdown', putChess)
     } else {
+      self.scene.resume()
       //移除滑鼠監聽或重新載入遊戲
     }
   })
   socket.on('drawChess', function(i, j) {
     updateCheckerboard(i, j)
   })
+  socket.on('winner', function(player) {
+    if (player === 'o') {
+      console.log('黑子獲勝')
+    } else {
+      console.log('白子獲勝')
+    }
+    self.scene.pause();
+    //停止渲染
+    //移除滑鼠監聽或重新載入遊戲
+  })
 }
 
 function updateCheckerboard(i, j) {
   //畫到畫布
-  if (isEnpty(i, j)) {
-    if (status === "o") {
-      checkerboard[i][j] = status
-      self.add.image(15 + j * 30, 15 + i * 30, 'black')
-    } else if (status == "x") {
-      checkerboard[i][j] = status
-      self.add.image(15 + j * 30, 15 + i * 30, 'white')
-    }
-    status = status === 'o' ? 'x' : 'o'
+  if (status === "o") {
+    self.add.image(15 + j * 30, 15 + i * 30, 'black')
+  } else if (status == "x") {
+    self.add.image(15 + j * 30, 15 + i * 30, 'white')
   }
-  console.log(checkerboard)
+  status = status === 'o' ? 'x' : 'o'
+
 }
 
 function putChess(pointer) {
@@ -86,132 +81,4 @@ function putChess(pointer) {
   let i = Math.floor(pointer.y / 30)
   let j = Math.floor(pointer.x / 30)
   socket.emit('putChess', i, j)
-}
-
-// function putChess(pointer) {
-//   let i = Math.floor(pointer.y / 30)
-//   let j = Math.floor(pointer.x / 30)
-//   if (isEnpty(i, j)) {
-//     if (status == "o") {
-//       checkerboard[i][j] = status
-//       self.add.image(15 + j * 30, 15 + i * 30, 'black')
-//     } else if (status == "x") {
-//       checkerboard[i][j] = status
-//       self.add.image(15 + j * 30, 15 + i * 30, 'white')
-//     }
-//
-//     if (horizontalWin(i, j) || straightWin(i, j) || rightOblique(i, j) || leftOblique(i, j)) {
-//       console.log(status + ' is fuck winner')
-//       console.log('fuck')
-//     }
-//     status = status === 'o' ? 'x' : 'o'
-//   }
-// }
-
-function isEnpty(i, j) {
-  return checkerboard[i][j] === 0
-}
-
-//左右
-function horizontalWin(i, j) {
-  let count = 1
-  for (let x = 1; x < 5; x++) {
-    if (checkerboard[i][j] === checkerboard[i][j - x]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  for (let x = 1; x < 5; x++) {
-    if (checkerboard[i][j] === checkerboard[i][j + x]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  return count >= 5
-}
-
-//上下
-function straightWin(i, j) {
-  let count = 1
-  for (let y = 1; y < 5; y++) {
-    if (i - y < 0) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i - y][j]) {
-      count += 1
-      // if (checkerboard[j][i - y - 1]) {
-      //   break
-      // }
-    } else {
-      break
-    }
-  }
-  for (let y = 1; y < 5; y++) {
-    if (i + y > 19) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i + y][j]) {
-      count += 1
-      // if (checkerboard[j][i + y + 1]) {
-      //   break
-      // }
-    } else {
-      break
-    }
-  }
-  return count >= 5
-}
-
-//右上到左下
-function rightOblique(i, j) {
-  let count = 1
-
-  for (let y = 1; y < 5; y++) {
-    if (i - y < 0) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i - y][j + y]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  for (let y = 1; y < 5; y++) {
-    if (i + y > 19) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i + y][j - y]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  return count >= 5
-}
-//左上到右下
-function leftOblique(i, j) {
-  let count = 1
-  for (let y = 1; y < 5; y++) {
-    if (i - y < 0) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i - y][j - y]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  for (let y = 1; y < 5; y++) {
-    if (i + y > 19) {
-      break
-    }
-    if (checkerboard[i][j] === checkerboard[i + y][j + y]) {
-      count += 1
-    } else {
-      break
-    }
-  }
-  return count >= 5
 }
